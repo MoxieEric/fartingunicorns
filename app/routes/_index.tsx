9ef063e1cc8710ec7d26408d7774cc3cadffe28b
Eric Nowels <ericnowels@gmail.com>
2024-10-27 14:41:52 -0600

    Init react remix (#1)
    * install react and remix deps
    
    * Boilerplate remix app with TS
    
    * basic styles and TX config
    
    * Unicorn icon
    
    * SImple home layout
    
    * configure netlify integration
    
    * rollup build binaries deps

---
+++ app/routes/_index.tsx
@@ -0,0 +1,24 @@
+import type { MetaFunction } from '@netlify/remix-runtime'
+import Unicorn from 'src/components/icons/Unicorn'
+
+export const meta: MetaFunction = () => {
+	return [
+		{ title: 'Farting Unicorns' },
+		{ name: 'description', content: 'Just because.' },
+	]
+}
+
+export default function Index() {
+	return (
+		<div className='flex h-screen items-center justify-center'>
+			<div className='flex flex-col items-center gap-16'>
+				<header className='flex flex-col items-center gap-4'>
+					<Unicorn className='w-32 h-32' />
+					<h1 className='text-6xl font-cherry bg-gradient-to-r from-pink-400  to-cyan-400 inline-block text-transparent bg-clip-text py-2'>
+						Farting Unicorns
+					</h1>
+				</header>
+			</div>
+		</div>
+	)
+}
